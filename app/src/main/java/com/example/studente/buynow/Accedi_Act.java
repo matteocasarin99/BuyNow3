e112b8036e7a1ce8924e53cbbdaba2895d38f1c6
matteocasarin <you@mer.md>
2018-02-22 10:08:18 +0100

    Inzio

---
+++ app/src/main/java/com/example/studente/buynow/Accedi_Act.java
@@ -0,0 +1,16 @@
+package com.example.studente.buynow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by studente on 22/02/2018.
+ */
+
+public class Accedi_Act extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interfaccia);
+
+    }
+}
