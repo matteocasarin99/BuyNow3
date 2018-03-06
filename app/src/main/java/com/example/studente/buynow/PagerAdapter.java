45f0c1b6fa03e5ad23cc95470803fe20d087ede8
studente <fdsfs.gh@me.it>
2018-03-06 13:00:01 +0100

    interfacfcia

---
+++ app/src/main/java/com/example/studente/buynow/PagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.studente.buynow;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by studente on 06/03/2018.
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0: {
+                TabFragment1 tab1 = new TabFragment1();
+
+            }
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
+}
---
+++ app/src/main/java/com/example/studente/buynow/TabFragment1.java
@@ -0,0 +1,18 @@
+package com.example.studente.buynow;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by studente on 06/03/2018.
+ */
+
+public class TabFragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.interfaccia, container, false);
+    }
+}
