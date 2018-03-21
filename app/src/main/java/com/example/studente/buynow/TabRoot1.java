6e1043834cf9c946e3107ce948db4fe53e6d85f1
studente <t.a@mfa.iu>
2018-03-21 12:49:04 +0100

    Creating product

--- app/src/main/java/com/example/studente/buynow/TabRoot1.java
+++ app/src/main/java/com/example/studente/buynow/TabRoot1.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -18,9 +19,20 @@ public class TabRoot1 extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.tab1root, container, false);
         ut=Accedi_ActRoot.ut;
-        EditText nomeProd=view.findViewById(R.id.txtNmPr);
+        final EditText nomeProd=view.findViewById(R.id.txtNmPr);
+        final EditText descProd=view.findViewById(R.id.txtDesc);
+        final EditText txtPrez=view.findViewById(R.id.txtPrez);
+        final EditText txtScon=view.findViewById(R.id.txtScon);
+        final EditText txtProv=view.findViewById(R.id.txtProv);
+        final EditText txtQuant=view.findViewById(R.id.txtQuant);
+        Button btnAgg=view.findViewById(R.id.btnAgg);
+        btnAgg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ut.addProdotto(new Prodotti(nomeProd.getText().toString(),descProd.getText().toString(),txtProv.getText().toString(),Double.parseDouble(txtPrez.getText().toString()),Double.parseDouble(txtScon.getText().toString()),Integer.parseInt(txtQuant.getText().toString())));
 
-
+            }
+        });
         return view;
     }
 }
