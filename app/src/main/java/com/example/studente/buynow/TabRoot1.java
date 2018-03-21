fd98820d04c995bd41caef3dfe13ac4698859c73
studente <t.a@mfa.iu>
2018-03-21 12:53:40 +0100

    Creating product

--- app/src/main/java/com/example/studente/buynow/TabRoot1.java
+++ app/src/main/java/com/example/studente/buynow/TabRoot1.java
@@ -1,5 +1,6 @@
 package com.example.studente.buynow;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Matteo on 20/03/2018.
@@ -29,8 +31,15 @@ public class TabRoot1 extends Fragment{
         btnAgg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ut.addProdotto(new Prodotti(nomeProd.getText().toString(),descProd.getText().toString(),txtProv.getText().toString(),Double.parseDouble(txtPrez.getText().toString()),Double.parseDouble(txtScon.getText().toString()),Integer.parseInt(txtQuant.getText().toString())));
-
+                if(nomeProd.getText().toString().compareTo("")==0 || descProd.getText().toString().compareTo("")==0 || txtPrez.getText().toString().compareTo("")==0 || txtProv.getText().toString().compareTo("")==0 || txtScon.getText().toString().compareTo("")==0 || txtQuant.getText().toString().compareTo("")==0){
+                    Context context = view.getContext();
+                    CharSequence text = "Compila i campi vuoti!!";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }else{
+                    ut.addProdotto(new Prodotti(nomeProd.getText().toString(),descProd.getText().toString(),txtProv.getText().toString(),Double.parseDouble(txtPrez.getText().toString()),Double.parseDouble(txtScon.getText().toString()),Integer.parseInt(txtQuant.getText().toString())));
+                }
             }
         });
         return view;
