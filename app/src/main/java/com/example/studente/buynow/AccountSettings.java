bc023c9c0d52378b5338fda4f5190692cffc0805
Matteo <brainbreakerz99@gmail.com>
2018-06-11 18:11:08 +0200

    Clear

--- app/src/main/java/com/example/studente/buynow/AccountSettings.java
+++ app/src/main/java/com/example/studente/buynow/AccountSettings.java
@@ -1,17 +1,23 @@
 package com.example.studente.buynow;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class AccountSettings extends AppCompatActivity {
     public Utenti_Password ut;
     int idUt;
     String password;
     private AdapterRow2 adapter;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,7 @@ public class AccountSettings extends AppCompatActivity {
         idUt = (Integer) getIntent().getExtras().getSerializable("Id");
         adapter = new AdapterRow2(getApplicationContext(), ut.arraylist_settings());
         password = (String) getIntent().getExtras().getSerializable("Password");
+        context=getApplicationContext();
         list.setAdapter(adapter);
         list.setFocusable(true);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -32,10 +39,27 @@ public class AccountSettings extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long i) {
                 if (ut.settings1().equals(parent.getItemAtPosition(position))) {
-                    System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                    AlertDialog.Builder miaAlert = new AlertDialog.Builder(getApplication());
+                    miaAlert.setMessage("Eliminare l'account?");
+                    miaAlert.setTitle("Attenzione!");
+
+                    miaAlert.setCancelable(false);
+                    miaAlert.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                           System.out.println("QUI SI");
+                        }
+                    });
+
+                    miaAlert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            System.out.println("QUI NO");
+                        }
+                    });
+
+                    AlertDialog alert = miaAlert.create();
+                    alert.show();
                 } else {
                     if (ut.settings2().equals(parent.getItemAtPosition(position))) {
-                        System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
                         Intent i3 = new Intent(AccountSettings.this, CambioPassword.class);
                         i3.putExtra("Utenti", ut);
                         i3.putExtra("id", idUt);
@@ -44,7 +68,20 @@ public class AccountSettings extends AppCompatActivity {
 
                     } else {
                         if (ut.settings3().equals(parent.getItemAtPosition(position))) {
-                            System.out.println("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+                            Context context = getApplicationContext();
+                            CharSequence text = "Logout In Corso...";
+                            int duration = Toast.LENGTH_SHORT;
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent i=new Intent(AccountSettings.this,MainActivity.class);
+                                    i.putExtra("Utenti",ut);
+                                    startActivity(i);
+                                    finish();
+                                }
+                            }, 1500);
                         }
                     }
                 }
